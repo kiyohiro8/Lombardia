bce5406180f534f745c674c67002b5b5f7b38193
kiyo hiro <hkiyo8@gmail.com>
2013-10-21 16:14:51 +0900

    AIのattack_priorityを整備

--- AI.py
+++ AI.py
@@ -19,48 +19,37 @@ def draw_priority(open_card, player, opponent, library, graveyard):
     else:
         pass
 
-def action_cpu(player, opponent, library, graveyard):
+def block_tend(player, opponent, library, graveyard):
 
+    block_tend = 1
+
+    #盲目思考
     if player.state == "redbull":
-        if player.hand.count("王子") == 3:
-            print("%sは3枚の王子を公開しました" %player.name)
-            player.point += 10
-        elif player.hand.count("司教") == 4:
-                print("%sは4枚の司教を公開しました(2点)" %player.name)
-                player.point += 2
-        elif player.hand.count("王子") == 2:
-            if player.hand.count("司教") == 3:
-                print("%sは2枚の王子と3枚の司教を公開しました(2点)" %player.name)
-                player.point += 2
-            elif player.hand.count("司教") == 2:
-                print("%sは2枚の王子と2枚の司教を公開しました(1点)" %player.name)
-                player.hand.count += 1
-            else:
-                pass
-        elif player.hand.count("司教") == 3:
-            print("%sは3枚の司教を公開しました(1点)" %player.name)
-            player.point += 1
+        if player.hand.count("騎士") >= 2:
+            block_tend = 0
+        elif player.hand.count("騎士") == 1 and player.hand.count("王子") >= 2:
+            block_tend = 0
         else:
             pass
 
-        if len(player.hand) > 7:
-            while len(player.hand) > 4:
-                if player.hand.count("貴族") >= 2:
-                    print("%sは貴族を%s枚捨てました(%i点)" %(player.name, player.hand.count("貴族"), player.hand.count("貴族")-1))
-                    player.point += player.hand.count("貴族") - 1
-                    for i in player.hand.count("貴族"):
-                        player.hand.remove("貴族")
-                        graveyard.append("貴族")
+def attack_priority(player, opponent, library, graveyard):
 
-                elif player.hand.count("騎士") >= 3:
-                    print("%sは騎士を3枚捨てて攻撃を行いました" %player.name)
-                    for i in range(3):
-                        player.hand.remove("騎士")
-                        graveyard.append("騎士")
+    card_list = ["王子", "司教", "騎士", "貴族"]
+    
+    #盲目思考
+    if player.state == "redbull":
+        if opponent.hand.count("王子") == 2:
+            return "王子"
+        elif opponent.hand.count("司教") >= 3:
+            return "司教"
+        else:
+            if opponent.hand.count("王子") == max(opponent.hand.count("王子"),opponent.hand.count("司教"),opponent.hand.count("騎士"),opponent.hand.count("貴族")):
+                return "王子"
+            elif opponent.hand.count("司教") == max(opponent.hand.count("王子"),opponent.hand.count("司教"),opponent.hand.count("騎士"),opponent.hand.count("貴族")):
+                return "司教"
+            elif opponent.hand.count("騎士") == max(opponent.hand.count("王子"),opponent.hand.count("司教"),opponent.hand.count("騎士"),opponent.hand.count("貴族")):
+                return "騎士"
+            else:
+                return "貴族"
 
-                else:
-                    print("%sは司教を1枚捨てました" %player.name)
-                    player.hand.remove("司教")
-                    graveyard.append("司教")
             
-                        
