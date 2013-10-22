f05f8dc541adec355568d100bfe9e01fc08e6b14
kiyo hiro <hkiyo8@gmail.com>
2013-10-22 16:23:40 +0900

    Player classに、revealプロパティを追加

--- player.py
+++ player.py
@@ -2,19 +2,20 @@
 
 class Player:
 
-    def __init__(self, name, ptype, hand, point, APNAP):
+    def __init__(self, name, ptype, hand, point, APNAP, reveal):
         
         self.name = name
         self.ptype = ptype
         self.hand = hand
         self.point = point
         self.APNAP = APNAP
+        self.reveal = reveal
 
     def showinfo(self):
-        print(self.point)
+        print("%s 手札:%i枚 勝利点: %i点" %(self.name, len(self.hand), self.point)
 
 class cpu(Player):
 
-    def __init__(self, name, ptype, hand, point, APNAP, state):
+    def __init__(self, name, ptype, hand, point, APNAP, reveal, state):
         Player.__init__(self, name, ptype, hand, point, APNAP)
         self.state = state
